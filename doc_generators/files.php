d73e7e064df0e7c9ed3c0250441efcbf46eb5cfa
Wayne Luke <wayneluke@me.com>
2018-01-11 08:35:05 -0800

    Add File List to track missing articles.

---
+++ doc_generators/files.php
@@ -0,0 +1,30 @@
+<?php
+
+// This should return false if there is something you want excluded
+function filter($file, $key, $iterator)
+{
+    $exclude = array('.git','images');
+    return ! in_array($file->getFilename(), $exclude);
+}
+
+// Recursive directory iterator for current directory, ignoring dots
+$it = new RecursiveDirectoryIterator($root, FilesystemIterator::SKIP_DOTS);
+// Wrapped by a filtering iterator with our filter function
+$it = new RecursiveCallbackFilterIterator($it, 'filter');
+// Wrapped by an iterator which automatically traverses children for us
+$it = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::SELF_FIRST);
+
+// And then just loop :)
+$path='';
+foreach($it as $file)
+{
+    if ($file->isdir()) {
+        $path = $file->getBasename();
+    } else {
+        $path = $file->getFilename();
+    }
+    echo str_repeat("\t", $it->getDepth())
+        . '- '
+        . $path
+        . PHP_EOL;
+}
