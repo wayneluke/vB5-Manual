1515506a881eaba2575a766b7cc5eabc607cc213
Wayne Luke <wayneluke@me.com>
2018-01-22 10:28:12 -0800

    Update files generator to output file automatically.

--- doc_generators/files.php
+++ doc_generators/files.php
@@ -1,9 +1,8 @@
 <?php
-// https://www.geekality.net/2014/04/20/php-simple-directory-recursion/
-
 $root='C:\wamp64\www\vbulletin\manual\user\pages';
 date_default_timezone_set ('America/Los_Angeles');
 
+// File lookup from https://www.geekality.net/2014/04/20/php-simple-directory-recursion/
 // This should return false if there is something you want excluded
 function filter($file, $key, $iterator)
 {
@@ -20,10 +19,10 @@ $it = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::SELF_FIRST);
 
 // And then just loop :)
 $path='';
-echo '# vBulletin 5 Connect File List' . PHP_EOL . 'MD Files with less than 200 bytes should be considered incomplete.' . PHP_EOL . PHP_EOL;
-
 $dirCount =0;
 $fileCount =0;
+ob_start();
+echo '# vBulletin 5 Connect File List' . PHP_EOL . 'MD Files with less than 200 bytes should be considered incomplete.' . PHP_EOL . PHP_EOL;
 foreach($it as $file)
 {
     $size ="";
@@ -46,6 +45,14 @@ foreach($it as $file)
     }
     echo str_repeat("\t", $it->getDepth()) . '- ' . $complete . ' ' . $path . $size . PHP_EOL;
 }
+
+echo PHP_EOL . PHP_EOL;
+echo 'Total Directories: ' . $dirCount . ' / Total Articles: ' . $fileCount;
+echo PHP_EOL . PHP_EOL;
+echo 'Updated at: ' . date('n/d/Y h:ia') . PHP_EOL;
+
+$fileTxt = ob_get_clean();
+file_put_contents('../files.md',$fileTxt);
 
 echo PHP_EOL . PHP_EOL;
 echo 'Total Directories: ' . $dirCount . ' / Total Articles: ' . $fileCount;
